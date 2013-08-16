6823647de54e1fa44fc063a7c79ad4ce48b23ac9
jbowkett <james_bowkett@lyagushka.co.uk>
2013-08-16 20:56:56 +0100

    simplified

--- src/main/java/info/bowkett/bowling/io/InvalidInputException.java
+++ src/main/java/info/bowkett/bowling/io/InvalidInputException.java
@@ -7,15 +7,9 @@ package info.bowkett.bowling.io;
  * Time: 8:04:45 AM
  * To change this template use File | Settings | File Templates.
  */
-public class InvalidInputException extends Throwable {
-  private final String advice;
+public class InvalidInputException extends Exception {
 
-  public InvalidInputException(String msg, String advice) {
+  public InvalidInputException(String msg) {
     super(msg);
-    this.advice = advice;
-  }
-
-  public String getAdvice() {
-    return advice;
   }
 }
