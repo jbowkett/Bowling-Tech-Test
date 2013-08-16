502e05c21b8558f73803f030906a26b3982c2fa9
jbowkett <james_bowkett@lyagushka.co.uk>
2013-08-16 09:31:52 +0100

    console abstraction

---
+++ src/main/java/info/bowkett/bowling/io/Console.java
@@ -0,0 +1,59 @@
+package info.bowkett.bowling.io;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jbowkett
+ * Date: Aug 16, 2013
+ * Time: 9:06:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Console {
+  private final java.io.Console console;
+
+  public Console(java.io.Console console) {
+    this.console = console;
+  }
+
+  public String getStringInput() throws QuitException {
+    String input = console.readLine();
+    if(input.equalsIgnoreCase("QUIT")){
+      throw new QuitException();
+    }
+    return input;
+  }
+
+  public int getIntInput(int minValue, int maxValue) throws QuitException, InvalidInputException {
+    final String input = getStringInput();
+    try{
+      final int parsedValue = Integer.parseInt(input);
+      if(withinBounds(minValue, maxValue, parsedValue)){
+        return parsedValue;
+      }
+      return outOfBoundsInputException(input, minValue, maxValue);
+    }
+    catch(NumberFormatException nfe){
+      return outOfBoundsInputException(input, minValue, maxValue);
+    }
+  }
+
+  private boolean withinBounds(int minValue, int maxValue, int parsedValue) {
+    return parsedValue >= minValue && parsedValue < maxValue;
+  }
+
+  private int outOfBoundsInputException(String input, int minValue, int maxValue) throws InvalidInputException {
+    throw new InvalidInputException("Invalid number input :["+input+"]", "Please enter a number between "+minValue+" & "+ maxValue);
+  }
+
+
+  public void msgln(String msg) {
+    System.out.println(msg);
+  }
+
+  public void msg(String msg) {
+    System.out.print(msg);
+  }
+
+  public void exception(InvalidInputException e) {
+    msgln(e.getMessage() + " :: " +e.getAdvice());
+  }
+}
---
+++ src/test/java/info/bowkett/bowling/io/ConsoleTest.java
@@ -0,0 +1,28 @@
+package info.bowkett.bowling.io;
+
+import org.junit.Test;
+import org.mockito.exceptions.base.MockitoException;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jbowkett
+ * Date: Aug 16, 2013
+ * Time: 9:19:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ConsoleTest {
+
+//  @Test(expected=QuitException.class)
+  //todo: this is unfortunate, mockito can't mock final classes...could use
+  //powermock instead, but I prefer the mockito given-when-then method naming
+  @Test(expected=MockitoException.class)
+  public void testQuitRaisesException() throws QuitException {
+    final java.io.Console wrapped = mock(java.io.Console.class);
+    when(wrapped.readLine()).thenReturn("quit");
+    final Console console = new Console(wrapped);
+    console.getStringInput();
+  }
+}
