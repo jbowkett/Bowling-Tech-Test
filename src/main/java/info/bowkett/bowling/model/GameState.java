9025cb56a5dd25fc70b40926499682244d2db1ac
jbowkett <james_bowkett@lyagushka.co.uk>
2013-08-19 00:12:16 +0100

    state + test

---
+++ src/main/java/info/bowkett/bowling/model/GameState.java
@@ -0,0 +1,87 @@
+package info.bowkett.bowling.model;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jbowkett
+ * Date: Aug 15, 2013
+ * Time: 11:57:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameState {
+  private Player[] players;
+  
+  public static final Turn GAME_OVER = new Turn(-1, -1, null, 0);
+  public static final Turn AWAITING_FIRST_TURN = new Turn(0, 0, null, 0);
+
+  private Turn currentTurn = AWAITING_FIRST_TURN;
+  private int currentPlayerIndex = 0;
+
+  private static int MAX_FRAMES = 10;
+
+  public void setPlayers(Player[] players) {
+    this.players = players;
+  }
+
+  public Turn getNextTurn() {
+    if(currentTurn == AWAITING_FIRST_TURN){
+      currentTurn = firstTurn();
+    }
+    else if(currentPlayerFinished()){
+      currentTurn = nextPlayer();
+    }
+    else{
+      currentTurn = samePlayerNextBall();
+    }
+    return currentTurn;
+  }
+
+  private boolean currentPlayerFinished() {
+    return wasPlayersLastBallInFrame() || wasStrike();
+  }
+
+  private boolean wasPlayersLastBallInFrame() {
+    return currentTurn.getBallNumber() == 2;
+  }
+
+  private boolean wasStrike() {
+    return currentTurn.getTally() == 10;
+  }
+
+  private Turn firstTurn() {
+    return new Turn(1, 1, players[currentPlayerIndex], 10);
+  }
+
+  private Turn samePlayerNextBall(){
+    return currentTurn.samePlayerNextBall();
+  }
+
+  private Turn nextPlayer(){
+    final int currentFrame = currentTurn.getFrameNumber();
+    if(frameComplete()){
+      return goToNextFrame(currentFrame);
+    }
+    else{
+      return new Turn(currentFrame, 1, players[++currentPlayerIndex], 10);
+    }
+  }
+
+  private Turn goToNextFrame(int currentFrame) {
+    goToFirstPlayer();
+    if(thereIsANextFrame(currentFrame)){
+      return new Turn(currentFrame + 1, 1, players[currentPlayerIndex], 10);
+    }
+    return GAME_OVER;
+  }
+
+  private boolean thereIsANextFrame(int currentFrame) {
+    return currentFrame < MAX_FRAMES;
+  }
+
+  private void goToFirstPlayer() {
+    currentPlayerIndex = 0;
+  }
+
+  private boolean frameComplete() {
+    return currentPlayerIndex+1 == players.length;
+  }
+}
---
+++ src/test/java/info/bowkett/bowling/model/GameStateTest.java
@@ -0,0 +1,188 @@
+package info.bowkett.bowling.model;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class GameStateTest {
+
+  @Test
+  public void testGameStartsWithOnePlayer(){
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(1);
+    state.setPlayers(players);
+    Turn next = state.getNextTurn();
+    assertEquals(10, next.getAvailablePins());
+    assertEquals(1, next.getBallNumber());
+    assertEquals(1, next.getFrameNumber());
+    assertEquals(players[0], next.getPlayer());
+  }
+
+  @Test
+  public void testCorrectTurnsWith_Frame_1_ball_2_WithOnePlayer(){
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(1);
+    state.setPlayers(players);
+    Turn ball_1 = state.getNextTurn();
+    Turn ball_2 = state.getNextTurn();
+    assertEquals(players[0], ball_2.getPlayer());
+  }
+
+  @Test
+  public void testCorrectTurnsWith_Frame_2_ball_1_WithOnePlayer(){
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(1);
+    state.setPlayers(players);
+    Turn ball_1 = state.getNextTurn();
+    Turn ball_2 = state.getNextTurn();
+    Turn ball_3 = state.getNextTurn();
+    assertEquals(players[0], ball_3.getPlayer());
+  }
+
+  @Test
+  public void testCorrectTurnsWith_Frame_1_ball_2_WithTwoPlayers(){
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(2);
+    state.setPlayers(players);
+    Turn ball_1 = state.getNextTurn();
+    Turn ball_2 = state.getNextTurn();
+    assertEquals(1, ball_2.getFrameNumber());
+    assertEquals(players[0], ball_2.getPlayer());
+  }
+
+
+  @Test
+  public void testCorrectTurnsWith_Frame_2_ball_1_WithTwoPlayers(){
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(2);
+    state.setPlayers(players);
+    Turn ball_1 = state.getNextTurn();
+    Turn ball_2 = state.getNextTurn();
+    Turn ball_3 = state.getNextTurn();
+    Turn ball_4 = state.getNextTurn();
+    Turn ball_5 = state.getNextTurn();
+    assertEquals(2, ball_5.getFrameNumber());
+    assertEquals(players[0], ball_5.getPlayer());
+  }
+
+  @Test
+  public void testCorrectTurnsWith_Frame_10_ball_1_WithTwoPlayers(){
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(2);
+    state.setPlayers(players);
+    for (int i = 0; i< 9; i++){
+      //player 1
+      Turn ball_1 = state.getNextTurn();
+      Turn ball_2 = state.getNextTurn();
+      //player 2
+      Turn ball_3 = state.getNextTurn();
+      Turn ball_4 = state.getNextTurn();
+    }
+    Turn ball_19 = state.getNextTurn();
+    assertEquals(10, ball_19.getFrameNumber());
+    assertEquals(1, ball_19.getBallNumber());
+    assertEquals(players[0], ball_19.getPlayer());
+  }
+
+  @Test
+  public void testCorrectFrameNumberFollowingAStrikeWithOnePlayer(){
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(1);
+    state.setPlayers(players);
+    Turn ball_1 = state.getNextTurn();
+    ball_1.setTally(10);
+    Turn ball_2 = state.getNextTurn();
+    assertEquals(2, ball_2.getFrameNumber());
+  }
+
+  @Test
+  public void testCorrectTurnsFollowingAStrikeWithTwoPlayers(){
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(2);
+    state.setPlayers(players);
+    Turn ball_1 = state.getNextTurn();
+    ball_1.setTally(10);
+    Turn ball_2 = state.getNextTurn();
+    assertEquals(players[1], ball_2.getPlayer());
+  }
+
+  @Test
+  public void testOnly10FramesArePlayedWithOnePlayer(){
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(1);
+    state.setPlayers(players);
+
+    for (int i = 1; i <= 10; i++){
+      Turn ball_1 = state.getNextTurn();
+      Turn ball_2 = state.getNextTurn();
+    }
+    Turn invalidTurn = state.getNextTurn();
+    assertEquals(GameState.GAME_OVER, invalidTurn);
+  }
+
+  @Test
+  public void testCorrectRemainingWithOnePlayer(){
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(1);
+    state.setPlayers(players);
+
+    Turn ball_1 = state.getNextTurn();
+    ball_1.setTally(3);
+    Turn ball_2 = state.getNextTurn();
+    assertEquals(7, ball_2.getAvailablePins());
+  }
+
+  @Test
+  public void testCorrectNumberOfBallsWithPerfectGameWithOnePlayer(){
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(1);
+    state.setPlayers(players);
+
+    for (int i = 0; i < 10; i++) {
+      final Turn t = state.getNextTurn();
+      t.setTally(10);
+    }
+    final Turn bonus_1 = state.getNextTurn();
+    assertEquals(false, GameState.GAME_OVER == bonus_1);
+
+    final Turn bonus_2 = state.getNextTurn();
+    assertEquals(false, GameState.GAME_OVER == bonus_2);
+  }
+
+  public void testCorrectTurnsWith_Frame_10_ball_2_WithTwoPlayers(){
+
+    final GameState state = new GameState();
+    final Player[] players = getPlayers(1);
+    state.setPlayers(players);
+
+
+  }
+
+  public void testCorrectTurnsWith_Frame_10_ball_3_WithTwoPlayersAndBothScoringStrikes(){
+
+  }
+
+  public void testCorrectTurnsWith_Frame_10_ball_3_WithTwoPlayersAndOnlyOneScoringAStrike(){
+
+  }
+
+
+
+
+
+  private Player[] getPlayers(int count) {
+    final Player[] players = new Player[count];
+    for (int i = 0; i < count; i++) {
+      players[i] = new Player("Player #"+i, new TallyCard(), new Scorer());
+    }
+    return players;
+  }
+}
+
