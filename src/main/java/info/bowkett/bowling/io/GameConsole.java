bcb8ad567b1e29b78a1c7fe7aa0f357297c8bd9a
jbowkett <james_bowkett@lyagushka.co.uk>
2013-08-18 22:02:37 +0100

    main game console

---
+++ src/main/java/info/bowkett/bowling/io/GameConsole.java
@@ -0,0 +1,41 @@
+package info.bowkett.bowling.io;
+
+import info.bowkett.bowling.model.Player;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jbowkett
+ * Date: Aug 15, 2013
+ * Time: 11:57:35 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GameConsole {
+  private final PlayerInputSubShell playerInputSubShell;
+  private final GameRecordingSubShell gameRecordingSubShell;
+  private final GameSummarySubShell gameSummarySubShell;
+  private final Console console;
+
+  public GameConsole(PlayerInputSubShell playerInputSubShell, GameRecordingSubShell gameRecordingSubShell, GameSummarySubShell gameSummarySubShell, Console console) {
+    this.playerInputSubShell = playerInputSubShell;
+    this.gameRecordingSubShell = gameRecordingSubShell;
+    this.gameSummarySubShell = gameSummarySubShell;
+    this.console = console;
+  }
+
+  public void start() {
+    printIntro();
+    try {
+      final Player[] players = playerInputSubShell.start();
+      gameRecordingSubShell.setPlayers(players);
+      gameRecordingSubShell.start();
+      gameSummarySubShell.start();
+    }
+    catch (QuitException e) {
+    }
+  }
+
+  private void printIntro() {
+    console.msgln("\n\n\nWelcome to the Bowling Scorecard!");
+    console.msgln("================================");
+  }
+}
