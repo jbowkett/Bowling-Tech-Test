e2ceb37a7c6b4a41301d9a4170aeb0ac960226e2
jbowkett <james_bowkett@lyagushka.co.uk>
2013-08-19 22:29:42 +0100

    refactored var name

--- src/main/java/info/bowkett/bowling/Main.java
+++ src/main/java/info/bowkett/bowling/Main.java
@@ -15,12 +15,12 @@ import info.bowkett.bowling.model.GameState;
 public class Main {
 
   public void scoreCard(){
-    final GameState constraints = new GameState();
+    final GameState gameState = new GameState();
     final Console console = new Console(System.console());
     final PlayerScorePrinter playerScorePrinter = new PlayerScorePrinter(console);
     final TeamScorePrinter teamScorePrinter = new TeamScorePrinter(console);
     final PlayerInputSubShell playerInputSubShell = new PlayerInputSubShell(console);
-    final GameRecordingSubShell gameRecordingSubShell = new GameRecordingSubShell(constraints, console);
+    final GameRecordingSubShell gameRecordingSubShell = new GameRecordingSubShell(gameState, console);
     final GameSummarySubShell gameSummarySubShell = new GameSummarySubShell(playerScorePrinter, teamScorePrinter, console);
     final GameConsole gameConsole = new GameConsole(playerInputSubShell, gameRecordingSubShell, gameSummarySubShell, console);
     gameConsole.start();
